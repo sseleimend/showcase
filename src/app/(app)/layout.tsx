2b13d9852207474e1e9f0245f750f7b8c2e00340
Victor Seleimend <sseleimend@gmail.com>
2025-06-29 11:00:59 -0300

    Add AppLayout component with sidebar and user authentication hooks

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useClerk, useUser } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+export default function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
+  const { signOut } = useClerk();
+  const { user } = useUser();
+
+  const handleLogoClick = () => {
+    router.push("/");
+  };
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
+  return <>{children}</>;
+}
